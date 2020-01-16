9167bffd9e8bec93ce8e659ca989e613c561e978
grntovspyn <grntovspyn@gmail.com>
2020-01-16 07:43:50 -0800

    phrase displaying

--- src/index.js
+++ src/index.js
@@ -2,6 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function IsSpace(props) {
+  
+  if(props.value === " ") {
+    return "hide";
+  } else {
+    return 'letter';
+  }
+
+}
+
+
+class PhraseLetters extends React.Component {
+render() {  
+
+  return (
+    <button 
+    
+      className="letter"
+    >
+      {this.props.value.toUpperCase()}
+  
+    </button>
+  );
+}
+
+}
+
+
 function Letter(props) {
     return (
       <button 
@@ -22,6 +50,7 @@ class Board extends React.Component {
 
     />;
   }
+  
 
   render() {
     const keyboardKeys = [
@@ -30,32 +59,10 @@ class Board extends React.Component {
       ["z","x","c","v","b","n","m"]
     ];
 
-    const selected = this.props.selected;
-    const phrase = "This is the phrase";
-    const splitPhrase = phrase.trim().toLowerCase().replace(/ /g,"").split("");
-
-    // https://gist.github.com/telekosmos/3b62a31a5c43f40849bb from post by VonD made on Jul, 15, 2016
-    const uniquePhrase = [ ...new Set(splitPhrase)];
-
-    const correctSelected = [];
-    const wrongSelected = [];
-
-    for(const letter of selected) {
-        if(uniquePhrase.indexOf(letter) >= 0){
-          correctSelected.push(letter);
-        } else {
-          wrongSelected.push(letter); 
-        }
-      }
        
     return (
       
       <div>
-        <div className="Selected">
-        {correctSelected}
-        <h1>Wrong</h1>
-        {wrongSelected}
-        </div>
           <div id="qwerty" className="section">
               {keyboardKeys.map((rows) =>
                   <div className="keyrow">
@@ -72,14 +79,89 @@ class Board extends React.Component {
   }
 }
 
+class Phrase extends React.Component {
+  renderPhrase(i) {
+   
+    return <PhraseLetters
+    value={i}
+    />;
+  }
+
+  checkSpace(i) {
+    return <IsSpace
+    value={i}
+    />;
+  }
+  
+  render() {
+
+    const phrase = this.props.phrase;
+    const splitPhrase = phrase.split("");
+
+    return (
+      <div className="section">
+      <div id="phrase" className="keyrow">
+        {splitPhrase.map((letters) =>
+          <div className={this.checkSpace(letters)}>
+          {this.renderPhrase(letters)}  
+          </div>
+        )}
+      </div>
+      </div>
+
+    );
+
+
+    /******************
+    const selected = this.props.selected;
+    const phrase = this.props.phrase;
+    const splitPhrase = phrase.trim().toLowerCase().replace(/ /g,"").split("");
+
+    // https://gist.github.com/telekosmos/3b62a31a5c43f40849bb from post by VonD made on Jul, 15, 2016
+    const uniquePhrase = [ ...new Set(splitPhrase)];
+
+    const correctSelected = [];
+    const wrongSelected = [];
+
+    for(const letter of selected) {
+        if(uniquePhrase.indexOf(letter) >= 0){
+          correctSelected.push(letter);
+        } else {
+          wrongSelected.push(letter); 
+        }
+      }
+    return;
+
+    }
+
+*/
+
+  }
+}
+
 class Game extends React.Component {
   constructor(props){
     super(props);
     this.state = {
 
+
       letters: [],
+      correctSelected: [],
+      wrongSelected: [],
 
     };
+  }
+
+  selectPhrase() {
+    const phrases = [
+      "This is phrase one",
+      "Second Phrase",
+      "Finally the third phrase"
+    ];
+
+
+    return phrases[Math.floor(Math.random() * phrases.length)]
+
   }
 
   handleClick(i) {
@@ -93,6 +175,12 @@ class Game extends React.Component {
   render() {
     return (
       <div className="game">
+        <Phrase 
+        phrase = {this.selectPhrase()}
+        
+        />
+        <div className="Phrase">
+        </div>
         <div className="game-board">
           <Board 
           selected = {this.state.letters}
